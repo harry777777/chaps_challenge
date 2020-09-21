40a105d164ef0580bb1c98a49e185e0903892e7f
OwenJemmett <sheepy.jemmett@gmail.com>
2020-09-21 13:47:00 +1200

    Added the application class

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Application.java
@@ -0,0 +1,13 @@
+package nz.ac.vuw.ecs.swen225.gp20.application;
+
+public class Application {
+    /**
+     * @author Owen
+     *
+     * main method, builds the gui
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
