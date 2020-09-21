e626a6c12d622b58404df6d518d5dd0341729e22
Marco <Marco@192.168.1.2>
2020-09-21 12:57:15 +1200

    Started renderer

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/Renderer.java
@@ -0,0 +1,20 @@
+package nz.ac.vuw.ecs.swen225.gp20.render;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+/**
+ * @author Marco
+ *
+ */
+public class Renderer {
+
+	
+	/**
+	 * @param g
+	 */
+	public void step(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+	}
+}
--- src/nz/ac/vuw/ecs/swen225/gp20/render/gitlabTest.java
+++
@@ -1,5 +0,0 @@
-package nz.ac.vuw.ecs.swen225.gp20.render;
-
-public class gitlabTest {
-	
-}
