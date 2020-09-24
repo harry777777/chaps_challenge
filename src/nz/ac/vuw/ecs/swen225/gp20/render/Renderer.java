fbe3e4c44ba54e90886b99b1ee653825bdf7b1b7
Marco <Marco@192.168.1.7>
2020-09-24 16:31:07 +1200

    Setup viewport class, started pulling information from Maze

--- src/nz/ac/vuw/ecs/swen225/gp20/render/Renderer.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/Renderer.java
@@ -7,6 +7,8 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.RoundRectangle2D;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.maze.actors.Player;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.FreeTile;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
 
 /**
@@ -18,13 +20,11 @@ import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
 public class Renderer {
 	
 	Maze maze;
+	ViewPort viewPort;
+	Inventory inventory;
 	int viewX, viewY, tileSize, viewWidth, viewHeight;
 	
-	//Chap
-	private static final Color CHAP_BODY = new Color(234, 222, 189);
 	
-	//Maze
-	private static final Color FLOOR_COLOR = new Color(164, 149, 126);
 	
 	/**
 	 * Initializes the renderer passing it the maze and drawing parameters
@@ -43,6 +43,8 @@ public class Renderer {
 		this.tileSize = tileSize;
 		this.viewHeight = height;
 		this.viewWidth = width;
+		this.viewPort = new ViewPort();
+		this.inventory = new Inventory();
 	}
 
 	
@@ -59,43 +61,16 @@ public class Renderer {
 		rh.put(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
 	    g2.setRenderingHints(rh);
 		
-	    
 	    //currently waiting on:
-	    //need access to the tiles in Maze, this is currently private and has no getter
-	    //need access to the Chap in Maze so I can draw him, as with the Maze tiles above
 	    //need this class and method to be implemented in Application so I can start drawing things and testing
 	    
-	    
-	    //find player position in the Maze and player offset
-	    
 		//access maze and draw
-	    //needs to be modified so it only access and draws the tiles around the player position as determined above
-	    //this is done using the viewHeight and viewWidth values as well as the position of the player
-	    Tile[][] tiles = new Tile[1][1]; //temp until I have access to the tiles in Maze
-	    for(int row = 0; row < tiles.length; row++) {
-	    	for(int col = 0; col < tiles[row].length; col++) {
-	    		//draw contents of tiles[row][col]
-	    		drawFloor(g2, viewX+row*tileSize, viewY+col*tileSize);
-	    	}
-	    }
+	    Tile[][] tiles = maze.getTiles();
+	    Player player = maze.getPlayer();
+	    viewPort.draw(g2, tiles, player, viewX, viewY, tileSize, viewWidth, viewHeight);
 	    
-	    //draw the player on top of the view using information from the previous two stages
-	    drawChap(g2, 0, 0); //temp
-	    
-	    //TODO setup the moving viewport
 	}
 	
-	private void drawChap(Graphics2D g2, int x, int y) {
-		//draw a rounded square as a temp representation of the chap
-		g2.setStroke(new BasicStroke(2));
-		g2.setColor(CHAP_BODY);
-		g2.fill(new RoundRectangle2D.Double(x+tileSize/4, y+tileSize/4, tileSize/2, tileSize/2, 10, 10));
-	}
 	
-	private void drawFloor(Graphics2D g2, int x, int y) {
-		//draw a plane color for the floor (temp)
-		g2.setColor(FLOOR_COLOR);
-		g2.fillRect(x, y, tileSize, tileSize);
-	}
 	
 }
--- src/nz/ac/vuw/ecs/swen225/gp20/render/ViewPort.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/ViewPort.java
@@ -1,5 +1,14 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.RoundRectangle2D;
+
+import nz.ac.vuw.ecs.swen225.gp20.maze.actors.Player;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.FreeTile;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
+
 /**
  * @author Marco
  * 
@@ -8,4 +17,57 @@ package nz.ac.vuw.ecs.swen225.gp20.render;
  */
 public class ViewPort {
 
+	
+	//Chap
+	private static final Color CHAP_BODY = new Color(234, 222, 189);
+		
+	//Maze
+	private static final Color FLOOR_COLOR = new Color(164, 149, 126);
+	
+	/**
+	 * Draws the view port
+	 * @param g2 
+	 * @param tiles 
+	 * @param player 
+	 * @param x 
+	 * @param y 
+	 * @param tileSize 
+	 * @param viewWidth 
+	 * @param viewHeight 
+	 */
+	public void draw(Graphics2D g2, Tile[][] tiles, Player player, int x, int y, int tileSize, int viewWidth, int viewHeight) {
+		
+		//find player position in the Maze and player offset
+		
+		//needs to be modified so it only access and draws the tiles around the player position as determined above
+	    //this is done using the viewHeight and viewWidth values as well as the position of the player
+		
+		for(int row = 0; row < tiles.length; row++) {
+	    	for(int col = 0; col < tiles[row].length; col++) {
+	    		Tile current = tiles[row][col];
+	    		if(current instanceof FreeTile) {
+	    			drawFloor(g2, x+row*tileSize, y+col*tileSize, tileSize);
+	    		}
+	    		
+	    	}
+	    }
+	    
+	    //draw the player on top of the view using information from the previous two stages
+	    drawChap(g2, 0, 0, tileSize); //temp
+	    
+	    //TODO setup the moving viewport
+	}
+	
+	private void drawChap(Graphics2D g2, int x, int y, int tileSize) {
+		//draw a rounded square as a temp representation of the chap
+		g2.setStroke(new BasicStroke(2));
+		g2.setColor(CHAP_BODY);
+		g2.fill(new RoundRectangle2D.Double(x+tileSize/4, y+tileSize/4, tileSize/2, tileSize/2, 10, 10));
+	}
+	
+	private void drawFloor(Graphics2D g2, int x, int y, int tileSize) {
+		//draw a plane color for the floor (temp)
+		g2.setColor(FLOOR_COLOR);
+		g2.fillRect(x, y, tileSize, tileSize);
+	}
 }
