9a5b808c449cce6b239ec6395e9aae47f121ee9b
OwenJemmett <sheepy.jemmett@gmail.com>
2020-09-25 11:57:49 +1200

    Testing stuff

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Application.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Application.java
@@ -1,5 +1,7 @@
 package nz.ac.vuw.ecs.swen225.gp20.application;
 
+import javax.swing.*;
+
 public class Application {
     /**
      * @author Owen
@@ -7,7 +9,27 @@ public class Application {
      * main method, builds the gui
      *
      */
+
+    private static JFrame frame;
+
     public static void main(String[] args) {
+        initialiseGui();
+
+    }
+    private static void initialiseGui(){
+        frame = new JFrame("Chads Challenge");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+
+        JMenu File = new JMenu("File");
+        JMenu Game = new JMenu("Game");
+
+        JMenuItem exit = new JMenuItem("Exit");
+        JMenuItem start = new JMenuItem("Start");
+
+        File.add(exit);
+        Game.add(start);
 
     }
 }
