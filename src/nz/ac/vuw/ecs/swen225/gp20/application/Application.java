878f48248301e016c0f62b237379f4ee22d242e4
OwenJemmett <sheepy.jemmett@gmail.com>
2020-09-25 14:20:30 +1200

    Updates to Application

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Application.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Application.java
@@ -1,6 +1,10 @@
 package nz.ac.vuw.ecs.swen225.gp20.application;
 
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.render.Renderer;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Application {
     /**
@@ -11,10 +15,12 @@ public class Application {
      */
 
     private static JFrame frame;
+    private final Maze maze = new Maze();   //Tile[][] tiles, Player player
+    private final Renderer renderer = new Renderer(maze);
+    private Graphics2D g;
 
     public static void main(String[] args) {
-        initialiseGui();
-
+        new Application();
     }
     private static void initialiseGui(){
         frame = new JFrame("Chads Challenge");
@@ -31,5 +37,19 @@ public class Application {
         File.add(exit);
         Game.add(start);
 
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(new Dimension(700, 700));
+
+    }
+    public void run(){
+        initialiseGui();
+
+        final double GAME_HERTZ = 60.0;
+        final double TBU = 1000000000 / GAME_HERTZ;
+        while(true){
+            renderer.draw(g);
+
+        }
     }
 }
