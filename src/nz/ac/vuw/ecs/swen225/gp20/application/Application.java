e89d1498eb08e6b9f1ee22647cc535ed112b9479
OwenJemmett <sheepy.jemmett@gmail.com>
2020-09-28 14:47:56 +1300

    60hz run method re added

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Application.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Application.java
@@ -20,7 +20,7 @@ import java.awt.event.*;
  *
  */
 
-public class Application {
+public class Application{
 
     private static JFrame frame;
     private final Maze maze = new Maze(new Tile[1][1], new Player(1,1));   //Tile[][] tiles, Player player
@@ -31,6 +31,7 @@ public class Application {
     private int timer = 60;
     private long TSLM = 0; // Time Since Last Move
     private boolean running;
+    private JComponent drawing;
 
     /**
      * @author Owen
@@ -40,6 +41,7 @@ public class Application {
      */
 
     public Application() {
+       // Gui gui = new Gui
         initialiseGui();
         run();
     }
@@ -70,15 +72,12 @@ public class Application {
         frame.setVisible(true);
         frame.setSize(new Dimension(700, 700));
 
-
-        frame.add();
-
         frame.addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {
                 if((e.getKeyCode() == 38) && !moving){
                     maze.movePlayer(Direction.UP);
-                  //  recordedFootage.getTickEvents().add(new TickEvent((System.currentTimeMillis() - TSLM), Direction.UP));
+                    recordedFootage.addEvent(new TickEvent(System.currentTimeMillis(), Direction.UP),System.currentTimeMillis());
                     TSLM = System.currentTimeMillis();
                 }
                 if((e.getKeyCode() == 40) && !moving){
@@ -99,6 +98,15 @@ public class Application {
             }
         });
 
+        // Set up drawing panel
+        drawing = new JComponent() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                renderer.draw(g);
+            }
+        };
+        drawing.setVisible(true);
+
     }
 
     /**
@@ -107,23 +115,75 @@ public class Application {
      * Application class, this runs the game loop and creates the GUI
      *
      */
-
     private void run() {
+        final double GAME_HERTZ = 60.0;      //Used this tutorial to setup a 60hz tick rate https://www.youtube.com/watch?v=LhUN3EKZiio&list=PLvJM9qNXoUYUDaDo_yfSKgn5dYnMmdN8B&index=2&t=335s
+        final double TBU = 1000000000 / GAME_HERTZ; // Time before update
+
+        final double MUBR = 1; //Most update before render
+
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+
+        final double TARGET_FPS = 60;
+        final double TTBR = 1000000000 / TARGET_FPS; // Total time before render
+
+        int frameCount = 0;
+        int i = 0;
+
+        while (true) {
+            double now = System.nanoTime();
+            int updateCount = 0;
+            while (((now - lastUpdateTime) > TBU) && updateCount < MUBR) {
+                update();
+                input();
+                lastUpdateTime += TBU;
+                updateCount++;
+            }
+
+            if (now - lastUpdateTime > TBU) {
+                lastUpdateTime = now - TBU;
+            }
+
+            redraw();
+            lastRenderTime = now;
+            frameCount++;
+
+            System.out.println(frameCount);
+
+            while (now - lastRenderTime < TTBR && now - lastUpdateTime < TBU) {
+                Thread.yield();
+                try {
+                    Thread.sleep(1);
+                } catch (Exception e) {
+                    System.out.println("yield error");
+                }
+                now = System.nanoTime();
+            }
+        }
+    }
+
+    private void redraw() {
+    }
+
+    /** private void run() {
         running = true;
         while(running){
             if(!(timer <= 0)) {
                 update();
-                try{Thread.sleep(1000);}catch (Exception e){
+               try{Thread.sleep(1000);}catch (Exception e){
                     System.out.println("Error when sleeping");
-                }
+               }
+                renderer.draw(g);
+                Graphics2D
                 timer--;
             }
         }
-    }
+    }*/
 
     private void input() {
     }
 
     private void update() {
+        recordedFootage.addEvent(new TickEvent(System.currentTimeMillis(), null), System.currentTimeMillis());
     }
 }
---
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Gui.java
@@ -0,0 +1,12 @@
+package nz.ac.vuw.ecs.swen225.gp20.application;
+
+public abstract class Gui {
+    public Gui(){initialise();}
+
+    protected void initialise(){
+
+    }
+
+  //  public abstract void redraw(Graphics g);
+
+}
