918b482af413b5855715d0a2f08d7efe870a685c
Marco <Marco@192.168.1.6>
2020-10-18 20:50:48 +1300

    Added temp visual distinction between key and treasure

--- src/nz/ac/vuw/ecs/swen225/gp20/render/ViewPort.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/ViewPort.java
@@ -8,6 +8,8 @@ import java.awt.geom.RoundRectangle2D;
 import java.awt.geom.Line2D;
 
 import nz.ac.vuw.ecs.swen225.gp20.maze.Player;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Treasure;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Key;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.FreeTile;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.WallTile;
@@ -109,7 +111,12 @@ public class ViewPort {
 	    			FreeTile currentT = (FreeTile) current;
 	    			drawFloor(g2, centerX-xMapOffset+row*tileSize-xOffset, centerY-yMapOffset+col*tileSize-yOffset, tileSize);
 	    			if(currentT.getItem() != null) {
-	    				drawItem(g2, centerX-xMapOffset+row*tileSize-xOffset, centerY-yMapOffset+col*tileSize-yOffset, tileSize);
+	    				if(currentT.getItem() instanceof Treasure) {
+	    					drawTreasure(g2, centerX-xMapOffset+row*tileSize-xOffset, centerY-yMapOffset+col*tileSize-yOffset, tileSize);
+	    				}
+	    				if(currentT.getItem() instanceof Key) {
+	    					drawKey(g2, centerX-xMapOffset+row*tileSize-xOffset, centerY-yMapOffset+col*tileSize-yOffset, tileSize);
+	    				}
 	    			}
 	    		}else if(current instanceof WallTile){
 	    			drawWall(g2, centerX-xMapOffset+row*tileSize-xOffset, centerY-yMapOffset+col*tileSize-yOffset, tileSize);
@@ -142,9 +149,14 @@ public class ViewPort {
 		g2.draw(new Rectangle2D.Double(x, y, tileSize, tileSize));
 	}
 	
-	private void drawItem(Graphics2D g2, double x, double y, int tileSize) {
+	private void drawTreasure(Graphics2D g2, double x, double y, int tileSize) {
 		g2.setColor(new Color(234, 222, 189));
 		g2.fill(new Ellipse2D.Double(x+tileSize/4, y+tileSize/4, tileSize/2, tileSize/2));
+	}
+	
+	private void drawKey(Graphics2D g2, double x, double y, int tileSize) {
+		g2.setColor(new Color(234, 222, 189));
+		g2.fill(new RoundRectangle2D.Double(x+tileSize/4, y+tileSize/4, tileSize/2, tileSize/2, 10, 10));
 	}
 	
 	private void drawX(Graphics2D g2, double x, double y, int tileSize) {
