0ace10dda0813ac4d181e2c1795d50a38f879fbb
Marco <Marco@192.168.1.6>
2020-10-19 01:02:30 +1300

    Cleaned up code and added javadoc

--- src/nz/ac/vuw/ecs/swen225/gp20/render/RenderTreasure.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/RenderTreasure.java
@@ -3,14 +3,16 @@ package nz.ac.vuw.ecs.swen225.gp20.render;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.Shape;
 import java.awt.TexturePaint;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 
-import nz.ac.vuw.ecs.swen225.gp20.maze.Player;
-
+/**
+ * @author Marco
+ * 
+ * Holds the logic for drawing treasures onto the view plane
+ *
+ */
 public class RenderTreasure {
 	
 	private static final Color CHAP_BODY_LIGHT = new Color(234, 222, 189);
@@ -37,6 +39,7 @@ public class RenderTreasure {
 	 * @param tileSize
 	 */
 	public void draw(Graphics2D g2, double x, double y, int tileSize){
+		
 		//do animation
 		if(up) {
 			if(height < 10) {
@@ -52,17 +55,15 @@ public class RenderTreasure {
 			}
 		}
 		
-
 		centerX = x+tileSize/2;
 		centerY = y+tileSize/2;
 		rightEdge = x+tileSize;
 		bottomEdge = y+tileSize;
 		
-		
 		BufferedImage bi = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_INT_RGB);
 		Graphics2D big = bi.createGraphics();
-		//Rectangle r = new Rectangle((int)x, (int)(y+height/2), tileSize, tileSize);
 		Rectangle r = new Rectangle(0, 0, tileSize, tileSize);
+		
 		big.setColor(CHAP_BODY_LIGHT);
 		big.fillRect(0, 0, tileSize, tileSize); //background color
 		
@@ -72,9 +73,8 @@ public class RenderTreasure {
 		TexturePaint tp = new TexturePaint(bi, r);
 		g2.setPaint(tp);
 		
-		//g2.fill(new Ellipse2D.Double(x+tileSize/4, y+tileSize/4+height/2, tileSize/2, tileSize/2)); //ball
 		
-		//push matrix - left arm
+		//push matrix - ball movement
 		Graphics2D gTemp = (Graphics2D) g2.create();
 		
 		g2.translate(x+tileSize/4, y+tileSize/4+height/2);
@@ -85,7 +85,7 @@ public class RenderTreasure {
 		g2.dispose();
 		g2 = (Graphics2D) gTemp.create();
 		
-		//push matrix - left arm
+		//push matrix - shadow scaling
 		gTemp = (Graphics2D) g2.create();
 		
 		g2.translate(x+tileSize/2-0.5, bottomEdge-tileSize/11);
@@ -93,14 +93,11 @@ public class RenderTreasure {
 		g2.scale(1+height/15, 1+height/50);
 		
 		g2.setColor(SHADOW);
-		g2.fill(new Ellipse2D.Double(-(tileSize/4)/2, 0, (tileSize/4), tileSize/14)); //shadow
+		g2.fill(new Ellipse2D.Double(-(tileSize/4)/2, 0, (tileSize/4), tileSize/13)); //shadow
 		
 		//pop matrix
 		g2.dispose();
 		g2 = (Graphics2D) gTemp.create();
-		
-		//g2.setColor(SHADOW);
-		//g2.fill(new Ellipse2D.Double(centerX-(tileSize/2.5)/2-(height/2)/2, bottomEdge-tileSize/11, (tileSize/2.5)+(height/2), tileSize/11)); //shadow
 
 
 	}
