a13a9fc0b01c9b2c5f92aa106e66f68cd987c8a7
harry777777 <68980798+harry777777@users.noreply.github.com>
2020-10-21 18:11:03 +1300

    changed replay to not return a null tickevent when its finished replaying

--- src/nz/ac/vuw/ecs/swen225/gp20/Record/Replay.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/Record/Replay.java
@@ -6,78 +6,85 @@ import nz.ac.vuw.ecs.swen225.gp20.persistence.JSONHandler;
 import java.io.IOException;
 
 /**
- * @author Harry
- * class responsible for replaying a recording of a recorded game.
+ * @author Harry class responsible for replaying a recording of a recorded game.
  */
 public class Replay {
-    private Recording recording;
-    private int recordingIndex;
-    private TickEvent currentTickEvent;
-    private boolean finished;
 
-    /**
-     * @param fileName the name of the recording file the replay will be loading
-     * @throws IOException
-     */
+  private Recording recording;
+  private int recordingIndex;
+  private TickEvent currentTickEvent;
+  private boolean finished;
 
-    public Replay(String fileName) throws IOException {
-        this.recording = loadRecording(fileName);
-        this.recordingIndex = 0;
-        this.currentTickEvent = this.recording.getTickEvents().get(0);
-        this.finished = false;
+  /**
+   * @param fileName the name of the recording file the replay will be loading
+   * @throws IOException
+   */
+
+  public Replay(String fileName) throws IOException {
+    this.recording = loadRecording(fileName);
+    this.recordingIndex = 0;
+    this.currentTickEvent = this.recording.getTickEvents().get(0);
+    this.finished = false;
+  }
+
+  /**
+   * @param fileName name of the Json file in recordings folder to load
+   * @return Recording of a game object loaded from a JSON file
+   * @throws IOException
+   */
+  private Recording loadRecording(String fileName) throws IOException {
+    JSONHandler<Recording> p = new JSONHandler<Recording>();
+    Recording R = p.read("recordings/" + fileName, Recording.class);
+    return R;
+  }
+
+  /**
+   * * @author Harry
+   * Returns the next Tickevent of a replay object and updates the position of the
+   * replay object in its replay process when finished will return null to alert application that
+   * the recording is over.
+   *
+   * @return TickEvent or null if the recording has finished.
+   */
+  public TickEvent getNextTick() {
+    if (finished) {
+      return currentTickEvent;
+    }
+    TickEvent t = this.currentTickEvent;
+    this.iterateReplay();
+    return t;
+
+  }
+
+  /**
+   * @author Harry
+   * Increment recording index, and update the current tickEvent that the recording is
+   * waiting at, Checks if the recording is at the end of the recording, updates the finished
+   * boolean if this is the case
+   */
+  private void iterateReplay() {
+    recordingIndex++;
+    if (recording.getTickEvents().size() > recordingIndex) {
+      this.currentTickEvent = recording.getTickEvents().get(recordingIndex);
+    } else {
+      this.finished = true;
     }
 
-    /**
-     * @param fileName name of the Json file in recordings folder to load
-     * @return Recording of a game object loaded from a JSON file
-     * @throws IOException
-     */
-    private Recording loadRecording(String fileName) throws IOException {
-        JSONHandler<Recording> p = new JSONHandler<Recording>();
-        Recording R = p.read("recordings/" + fileName , Recording.class);
-        return R;
-    }
-     /**
-      * * @author Harry
-     * Returns the next Tickevent of a replay object and updates the position of the replay object in its replay process
-      * when finished will return null to alert application that the recording is over.
-      * @return  TickEvent or null if the recording has finished.
-     *
-      */
-    public TickEvent getNextTick(){
-        if(finished){
-            return null;
-        }
-        TickEvent t = this.currentTickEvent;
-        this.iterateReplay();
-        return t;
+  }
 
-    }
+  /**
+   * @return Boolean finished, whether the replay is finished replaying
+   */
+  public boolean isFinished() {
+    return this.finished;
+  }
 
-    /**
-     * @author Harry
-     * Increment recording index, and update the current tickEvent that the recording is waiting at,
-     * Checks if the recording is at the end of the recording, updates the finished boolean if this is the case
-     */
-    private void iterateReplay() {
-        recordingIndex++;
-        if(recording.getTickEvents().size()>recordingIndex) {
-            this.currentTickEvent = recording.getTickEvents().get(recordingIndex);
-        }
-        else{ this.finished = true;
-        }
-
-    }
-    public boolean isFinished(){
-        return this.finished;
-    }
-
-    /** @author Harry
-     * A test method that prints a recording object to the console
-     */
-    public void testRecording() {
-        System.out.println(this.recording.toString());
-    }
+  /**
+   * @author Harry A test method that prints a recording object to the console
+   */
+  public void testRecording() {
+    System.out.println(this.recording.toString());
+  }
 }
 
 
