0ff6c082b4b62746113b1963730450a1fe353c78
Marco <Marco@192.168.1.6>
2020-10-22 18:50:11 +1300

    Groundwork for exit door

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/RenderExitDoor.java
@@ -0,0 +1,10 @@
+package nz.ac.vuw.ecs.swen225.gp20.render;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class RenderExitDoor {
+	public void draw(Graphics2D g2, double x, double y, int tileSize, boolean locked){
+		
+	}
+}
